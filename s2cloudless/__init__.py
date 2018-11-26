1f4ef470a6605a588e9e812b2ae75d69918fec76
AleksMat <matej.aleksandrov@sinergise.com>
2018-11-26 17:07:05 +0100

    added s2 evalscript to init

--- s2cloudless/__init__.py
+++ s2cloudless/__init__.py
@@ -2,7 +2,7 @@
 This module lists all externally useful classes and functions
 """
 
-from .S2PixelCloudDetector import S2PixelCloudDetector, CloudMaskRequest, MODEL_EVALSCRIPT
+from .S2PixelCloudDetector import S2PixelCloudDetector, CloudMaskRequest, MODEL_EVALSCRIPT, S2_BANDS_EVALSCRIPT
 from .PixelClassifier import PixelClassifier
 from .test_cloud_detector import test_sentinelhub_cloud_detector
 
