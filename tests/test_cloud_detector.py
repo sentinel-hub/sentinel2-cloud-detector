fb7be9f8e3e1bbf87ac66e937e0e2963fbd4bb39
AleksMat <matej.aleksandrov@sinergise.com>
2018-11-28 11:33:53 +0100

    added additional test for checking deepcopy of request class was sucessful

--- tests/test_cloud_detector.py
+++ tests/test_cloud_detector.py
@@ -35,25 +35,24 @@ class TestCloudMaskRequest(TestS2Cloudless):
 
         bbox1 = BBox([-90.9216499, 14.4190528, -90.8186531, 14.5520163], crs=CRS.WGS84)  # From examples
         bbox2 = BBox([46.16, -16.15, 46.51, -15.58], crs=CRS.WGS84)  # From sentinelhub-py examples
-        custom_url_params = {
+        cls.custom_url_params = {
             CustomUrlParam.SHOWLOGO: True,
             CustomUrlParam.TRANSPARENT: False,
             CustomUrlParam.EVALSCRIPT: 'return [B01]',
             CustomUrlParam.ATMFILTER: 'DOS1'
         }
 
-        wms_request = WmsRequest(layer='BANDS-S2-L1C', bbox=bbox1, time=('2017-12-01', '2017-12-31'), width=60,
-                                 height=None, image_format=MimeType.TIFF, custom_url_params=custom_url_params,
-                                 instance_id=cls.CONFIG.instance_id)
-        wcs_request = WcsRequest(layer='TRUE-COLOR-S2-L1C', bbox=bbox2, time='2016-07-18T07:14:04',
-                                 resx='100m', resy='100m',
-                                 image_format=MimeType.PNG, data_folder='.')
+        cls.wms_request = WmsRequest(layer='BANDS-S2-L1C', bbox=bbox1, time=('2017-12-01', '2017-12-31'), width=60,
+                                     height=None, image_format=MimeType.TIFF, custom_url_params=cls.custom_url_params,
+                                     instance_id=cls.CONFIG.instance_id)
+        cls.wcs_request = WcsRequest(layer='TRUE-COLOR-S2-L1C', bbox=bbox2, time='2016-07-18T07:14:04',
+                                     resx='100m', resy='100m', image_format=MimeType.PNG, data_folder='.')
 
         cls.test_cases = [
-            TestCaseContainer('WMS', CloudMaskRequest(wms_request, threshold=0.6, average_over=2, dilation_size=5),
+            TestCaseContainer('WMS', CloudMaskRequest(cls.wms_request, threshold=0.6, average_over=2, dilation_size=5),
                               clm_min=0, clm_max=1, clm_mean=0.343827, clm_median=0, clp_min=0.00011, clp_max=0.99999,
                               clp_mean=0.23959, clp_median=0.01897, mask_shape=(7, 81, 60)),
-            TestCaseContainer('WCS, partial no data', CloudMaskRequest(wcs_request, all_bands=True), clm_min=0,
+            TestCaseContainer('WCS, partial no data', CloudMaskRequest(cls.wcs_request, all_bands=True), clm_min=0,
                               clm_max=1, clm_mean=0.04468, clm_median=0, clp_min=-50.0, clp_max=0.999635,
                               clp_mean=-7.72785, clp_median=0.011568, mask_shape=(1, 634, 374))
         ]
@@ -75,6 +74,15 @@ class TestCloudMaskRequest(TestS2Cloudless):
                                  exp_min=test_case.clp_min, exp_max=test_case.clp_max,
                                  exp_mean=test_case.clp_mean, exp_median=test_case.clp_median,
                                  test_name=test_case.name, delta=1e-4)
+
+    def test_requests_unchanged(self):
+        self.assertEqual(self.wms_request.custom_url_params, self.custom_url_params,
+                         msg='Custom url params were changed')
+        self.assertTrue(self.wms_request.custom_url_params[CustomUrlParam.SHOWLOGO],
+                        msg='Custom url params were changed')
+
+        self.assertEqual(self.wms_request.image_format, MimeType.TIFF, msg='Image format of WMS class was changed')
+        self.assertEqual(self.wcs_request.image_format, MimeType.PNG, msg='Image format of WCS class was changed')
 
 
 if __name__ == '__main__':
