378b7d834cbeb3aaee6f706bc5d3d25345d3f397
AleksMat <matej.aleksandrov@sinergise.com>
2019-11-25 16:46:48 +0100

    updated and fixed some docs strings

--- s2cloudless/PixelClassifier.py
+++ s2cloudless/PixelClassifier.py
@@ -14,24 +14,20 @@ class PixelClassifier:
     Pixel classifier divides the image into individual pixels, runs classifier over
     them, and finally produces a classification mask of the same size as image.
 
-    The classifier can be of any type as long as it has the following two methods
-    implemented:
+    The classifier can be of a type that is explicitly supported (e.g. lightgbm.Booster) or of any type as long as
+    it has the following two methods implemented:
         - predict(X)
         - predict_proba(X)
 
     This is true for all classifiers that follow scikit-learn's API.
     The APIs of scikit-learn's objects is described
     at: http://scikit-learn.org/stable/developers/contributing.html#apis-of-scikit-learn-objects.
-
-    :param classifier: trained classifier that will be executed over an entire image
-    :type classifier: any classifier with predict(X) and predict_proba(X) methods
     """
     # pylint: disable=invalid-name
     def __init__(self, classifier):
         """
-        :param classifier: An instance of classification object which should have a method predict and either a method
-            predict_proba or an option to call predict(data, raw_score=True)
-        :type classifier: object
+        :param classifier: An instance of trained classifier that will be executed over an entire image
+        :type classifier: Booster or object that implements methods predict and predict_proba
         """
         self._check_classifier(classifier)
         self.classifier = classifier
@@ -39,7 +35,7 @@ class PixelClassifier:
     @staticmethod
     def _check_classifier(classifier):
         """
-        Check if the classifier implements predict method
+        Checks if the classifier is of correct type or if it implements predict and predict_proba methods
         """
         if isinstance(classifier, Booster):
             return
@@ -54,7 +50,7 @@ class PixelClassifier:
 
     @staticmethod
     def extract_pixels(X):
-        """ Extract pixels from array X
+        """ Extracts pixels from array X
 
         :param X: Array of images to be classified.
         :type X: numpy array, shape = [n_images, n_pixels_y, n_pixels_x, n_bands]
@@ -66,13 +62,13 @@ class PixelClassifier:
             raise ValueError('Array of input images has to be a 4-dimensional array of shape'
                              '[n_images, n_pixels_y, n_pixels_x, n_bands]')
 
-        new_shape = (X.shape[0] * X.shape[1] * X.shape[2], X.shape[3],)
+        new_shape = X.shape[0] * X.shape[1] * X.shape[2], X.shape[3]
         pixels = X.reshape(new_shape)
         return pixels
 
     def image_predict(self, X):
         """
-        Predicts class label for the entire image.
+        Predicts class labels for the entire image.
 
         :param X: Array of images to be classified.
         :type X: numpy array, shape = [n_images, n_pixels_y, n_pixels_x, n_bands]
@@ -96,7 +92,6 @@ class PixelClassifier:
 
         :param X: Array of images to be classified.
         :type X: numpy array, shape = [n_images, n_pixels_y, n_pixels_x, n_bands]
-
         :return: classification probability map
         :rtype: numpy array, [n_samples, n_pixels_y, n_pixels_x]
         """
