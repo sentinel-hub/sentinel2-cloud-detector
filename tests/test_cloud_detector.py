7850f6a832d9790178a0f82e6e9426f1cd4b4c2b
maleksandrov <matej.aleksandrov@sinergise.com>
2021-03-05 17:06:22 +0100

    added extra test for single-temporal data

--- tests/test_cloud_detector.py
+++ tests/test_cloud_detector.py
@@ -20,6 +20,12 @@ def test_pixel_cloud_detector():
     assert np.allclose(cloud_probs, data['cl_probs'], atol=1e-14)
     assert np.array_equal(cloud_mask, data['cl_mask'])
 
+    single_temporal_cloud_probs = cloud_detector.get_cloud_probability_maps(data['s2_im'][0, ...])
+    single_temporal_cloud_mask = cloud_detector.get_cloud_masks(data['s2_im'][0, ...])
+
+    assert np.array_equal(single_temporal_cloud_probs, cloud_probs[0, ...])
+    assert np.array_equal(single_temporal_cloud_mask, cloud_mask[0, ...])
+
     cloud_detector = S2PixelCloudDetector(all_bands=False)
     with pytest.raises(ValueError):
         cloud_detector.get_cloud_probability_maps(data['s2_im'])
