8a42c1ae305874a3f96d32cb682a16e01d40c921
jgersak <jan.gersak@sinergise.com>
2023-02-06 12:05:24 +0100

    Remove sentinelhub_masking
    remove sentinelhub_masking
    remove sentinelhub_masking tests
    add get_timestemps, add download_bands_and_valid_data_mask
    add tests

--- s2cloudless/utils.py
+++ s2cloudless/utils.py
@@ -1,5 +1,21 @@
 """Module providing various utilities."""
 
+import datetime as dt
+from typing import List, Optional, Tuple
+
+import numpy as np
+
+from sentinelhub import (
+    BBox,
+    DataCollection,
+    MimeType,
+    SentinelHubCatalog,
+    SentinelHubDownloadClient,
+    SentinelHubRequest,
+    SHConfig,
+    filter_times,
+)
+
 S2_BANDS = ["B01", "B02", "B03", "B04", "B05", "B06", "B07", "B08", "B8A", "B09", "B10", "B11", "B12"]
 MODEL_BAND_IDS = [0, 1, 3, 4, 7, 8, 9, 10, 11, 12]
 
@@ -54,3 +70,83 @@ def get_s2_evalscript(all_bands: bool = False, reflectance: bool = False) -> str
         output_sample_type="FLOAT32" if reflectance else "UINT16",
         metadata_evalscript="" if reflectance else METADATA_EVALSCRIPT_TEMPLATE,
     ).strip("\n ")
+
+
+def get_timestemps(
+    bbox: BBox,
+    time_interval: Tuple[dt.datetime, dt.datetime],
+    *,
+    maxcc: Optional[float] = None,
+    data_collection: DataCollection = DataCollection.SENTINEL2_L1C,
+    config: Optional[SHConfig] = None,
+    time_difference: dt.timedelta = None,
+) -> List[dt.datetime]:
+    """Get the list of timestamps from within date range for which data of the bbox is available."""
+    time_difference = time_difference if time_difference else dt.timedelta(seconds=0)
+
+    catalog = SentinelHubCatalog(config=config)
+    cloud_cover_query = None
+    if maxcc is not None:
+        cloud_cover_query = f"eo:cloud_cover < {100 * float(maxcc)}"
+
+    search_iterator = catalog.search(
+        data_collection,
+        bbox=bbox,
+        time=time_interval,
+        filter=cloud_cover_query,
+        fields={
+            "include": [
+                "properties.datetime",
+            ],
+            "exclude": [],
+        },
+    )
+
+    return filter_times(search_iterator.get_timestamps(), time_difference)
+
+
+def download_bands_and_valid_data_mask(
+    bbox: BBox,
+    timestamps: List[dt.datetime],
+    *,
+    data_collection: DataCollection = DataCollection.SENTINEL2_L1C,
+    config: Optional[SHConfig] = None,
+    size: Optional[Tuple[int, int]] = None,
+    resolution: Optional[Tuple[float, float]] = None,
+    all_bands: bool = True,
+) -> Tuple[np.ndarray, np.ndarray]:
+    """Downloaded specifid data"""
+
+    client = SentinelHubDownloadClient(config=config)
+
+    api_requests = []
+    for timestamp in timestamps:
+        request = SentinelHubRequest(
+            evalscript=get_s2_evalscript(all_bands=all_bands, reflectance=False),
+            input_data=[
+                SentinelHubRequest.input_data(
+                    data_collection=data_collection,
+                    time_interval=(timestamp, timestamp),
+                )
+            ],
+            responses=[
+                SentinelHubRequest.output_response("default", MimeType.TIFF),
+                SentinelHubRequest.output_response("userdata", MimeType.JSON),
+            ],
+            bbox=bbox,
+            size=size,
+            resolution=resolution,
+            config=config,
+        )
+
+        api_requests.append(request)
+
+    responses = client.download([api_request.download_list[0] for api_request in api_requests])
+
+    # pylint: disable=not-an-iterable
+    data = np.asarray([response["default.tif"] for response in responses], dtype=np.float32)
+    norm_factors = [response["userdata.json"]["norm_factor"] for response in responses]
+    del responses
+
+    normalized_bands = (np.round(array * factor, 4) for array, factor in zip(data[..., :-1], norm_factors))
+    return np.asarray(list(normalized_bands), dtype=np.float32), data[..., -1] != 0
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,9 +1,32 @@
 """
 Tests for utils.py module
 """
+import datetime as dt
+import os
+from typing import List, Tuple
+
+import numpy as np
 import pytest
 
-from s2cloudless import get_s2_evalscript
+from sentinelhub import CRS, BBox, SHConfig
+
+from s2cloudless.utils import download_bands_and_valid_data_mask, get_s2_evalscript, get_timestemps
+
+BBOX1 = BBox([-90.9216499, 14.4190528, -90.8186531, 14.5520163], crs=CRS.WGS84)
+BBOX2 = BBox(((624024.4, 8214123.1), (661906.6, 8276948.7)), crs=CRS(32738))
+DAY = dt.timedelta(seconds=24 * 60 * 60)
+
+
+@pytest.fixture(name="config")
+def config_fixture() -> SHConfig:
+    config = SHConfig()
+
+    for param in config.get_params():
+        env_variable = param.upper()
+        if os.environ.get(env_variable):
+            setattr(config, param, os.environ.get(env_variable))
+
+    return config
 
 
 @pytest.mark.parametrize("all_bands", [True, False])
@@ -20,3 +43,67 @@ def test_get_s2_evalscript(all_bands: bool, reflectance: bool) -> None:
     assert input_units_str in evalscript
     output_sample_type_str = 'sampleType: "FLOAT32"' if reflectance else 'sampleType: "UINT16"'
     assert output_sample_type_str in evalscript
+
+
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [
+        ({"bbox": BBOX1, "time_interval": (dt.datetime(2021, 1, 1), dt.datetime(2021, 1, 1))}, []),
+        (
+            {"bbox": BBOX1, "time_interval": (dt.datetime(2021, 1, 1), dt.datetime(2021, 1, 10))},
+            [
+                dt.datetime(2021, 1, 4, 16, 39, 29),
+                dt.datetime(2021, 1, 4, 16, 39, 44),
+                dt.datetime(2021, 1, 9, 16, 39, 29),
+                dt.datetime(2021, 1, 9, 16, 39, 43),
+            ],
+        ),
+        (
+            {
+                "bbox": BBOX1,
+                "time_interval": (dt.datetime(2021, 1, 1), dt.datetime(2021, 1, 10)),
+                "time_difference": dt.timedelta(seconds=600),
+            },
+            [dt.datetime(2021, 1, 4, 16, 39, 29), dt.datetime(2021, 1, 9, 16, 39, 29)],
+        ),
+    ],
+)
+def test_get_timestemps(test_input: dict, config: SHConfig, expected: List[dt.datetime]) -> None:
+    timestemps = get_timestemps(**test_input, config=config)
+    assert len(timestemps) == len(expected)
+    assert all([val.replace(tzinfo=None) == exp for exp, val in zip(expected, timestemps)])
+
+
+@pytest.mark.parametrize(
+    "test_input, expected_shape",
+    [
+        ({"bbox": BBOX1, "timestamps": [dt.datetime(2021, 1, 4)], "size": (60, 81), "all_bands": True}, (1, 81, 60)),
+        (
+            {
+                "bbox": BBOX1,
+                "timestamps": [dt.datetime(2021, 1, 4, 16, 39, 29), dt.datetime(2021, 1, 9, 16, 39, 29)],
+                "size": (60, 81),
+                "all_bands": True,
+            },
+            (2, 81, 60),
+        ),
+        (
+            {
+                "bbox": BBOX1,
+                "timestamps": [dt.datetime(2021, 1, 4, 16, 39, 29), dt.datetime(2021, 1, 9, 16, 39, 29)],
+                "size": (60, 81),
+                "all_bands": False,
+            },
+            (2, 81, 60),
+        ),
+    ],
+)
+def test_download_bands_and_valid_data_mask(
+    test_input: dict, config: SHConfig, expected_shape: Tuple[int, int, int]
+) -> None:
+    bands, mask = download_bands_and_valid_data_mask(**test_input, config=config)
+    band_num = 10 if not test_input["all_bands"] else 13
+    assert bands.shape == expected_shape + (band_num,)
+    assert bands.dtype == np.float32
+    assert mask.shape == expected_shape
+    assert mask.dtype == bool
