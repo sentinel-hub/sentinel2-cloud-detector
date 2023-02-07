cd94792e3096cdf039e509b34d384865f66e856c
jgersak <jan.gersak@sinergise.com>
2023-02-07 09:11:16 +0100

    remove pylint comment

--- s2cloudless/utils.py
+++ s2cloudless/utils.py
@@ -106,7 +106,6 @@ def get_timestamps(
     return filter_times(search_iterator.get_timestamps(), time_difference)
 
 
-# pylint: disable-msg=too-many-locals
 def download_bands_and_valid_data_mask(
     bbox: BBox,
     timestamps: List[dt.datetime],
