5f7cab6ca54a9b93a8db69f7f7168059f5dcb409
Matic Lubej <matic.lubej@sinergise.com>
2023-04-05 11:32:15 +0200

    Use evalscript generation and timestamp availability utilities from sh-py (#69)
    * remove old function from init
    
    * use new evalscript generation utility
    
    * fix config issue in test
    
    * remove evalscript generation test, ported to sh-py
    
    * update evalscript utility parameter name
    
    * remove custom timestamps utility and tests, use the one from sh-py
    
    * update example notebook
    
    * bump sh-py version

--- s2cloudless/utils.py
+++ s2cloudless/utils.py
@@ -6,105 +6,12 @@ from typing import List, Optional, Tuple
 import cv2
 import numpy as np
 
-from sentinelhub import (
-    BBox,
-    DataCollection,
-    MimeType,
-    SentinelHubCatalog,
-    SentinelHubDownloadClient,
-    SentinelHubRequest,
-    SHConfig,
-    filter_times,
-)
+from sentinelhub import BBox, DataCollection, MimeType, SentinelHubDownloadClient, SentinelHubRequest, SHConfig
+from sentinelhub.evalscript import generate_evalscript
 
 S2_BANDS = ["B01", "B02", "B03", "B04", "B05", "B06", "B07", "B08", "B8A", "B09", "B10", "B11", "B12"]
 MODEL_BAND_IDS = [0, 1, 3, 4, 7, 8, 9, 10, 11, 12]
-
-DATA_EVALSCRIPT_TEMPLATE = """
-//VERSION=3
-function setup() {{
-  return {{
-    input: [{{
-      bands: [{bands}],
-      units: "{input_units}"
-    }}],
-    output: {{
-      id: "bands",
-      bands: {band_number},
-      sampleType: "{output_sample_type}"
-    }}
-  }};
-}}
-{metadata_evalscript}
-function evaluatePixel(sample) {{
-  return [{sample_bands}];
-}}
-"""
-
-METADATA_EVALSCRIPT_TEMPLATE = """
-function updateOutputMetadata(scenes, inputMetadata, outputMetadata) {
-  outputMetadata.userData = {
-    "norm_factor":  inputMetadata.normalizationFactor
-  }
-}
-"""
-
-
-def get_s2_evalscript(all_bands: bool = False, reflectance: bool = False) -> str:
-    """Provides an `s2cloudless` suited evalscript to download Sentinel-2 data
-
-    :param all_bands: If `True` the evalscript will use all bands. Otherwise it will use only bands needed for cloud
-        masking.
-    :param reflectance: If `True` the evalscript will define reflectance values. Otherwise it will use digital
-        numbers together with normalization factors to rescale them.
-    :return: An evalscript to be used with the `sentinelhub` package.
-    """
-    bands = S2_BANDS
-    if not all_bands:
-        bands = [bands[index] for index in MODEL_BAND_IDS]
-    bands = bands + ["dataMask"]
-
-    return DATA_EVALSCRIPT_TEMPLATE.format(
-        bands=", ".join(f'"{band}"' for band in bands),
-        sample_bands=", ".join(f"sample.{band}" for band in bands),
-        band_number=len(bands),
-        input_units="reflectance" if reflectance else "DN",
-        output_sample_type="FLOAT32" if reflectance else "UINT16",
-        metadata_evalscript="" if reflectance else METADATA_EVALSCRIPT_TEMPLATE,
-    ).strip("\n ")
-
-
-def get_timestamps(
-    bbox: BBox,
-    time_interval: Tuple[dt.datetime, dt.datetime],
-    *,
-    maxcc: Optional[float] = None,
-    data_collection: DataCollection = DataCollection.SENTINEL2_L1C,
-    config: Optional[SHConfig] = None,
-    time_difference: Optional[dt.timedelta] = None,
-) -> List[dt.datetime]:
-    """Get the list of timestamps for which data is available. Takes into account the bbox and time interval."""
-    time_difference = time_difference if time_difference else dt.timedelta(seconds=0)
-
-    catalog = SentinelHubCatalog(config=config)
-    cloud_cover_query = None
-    if maxcc is not None:
-        cloud_cover_query = f"eo:cloud_cover < {100 * float(maxcc)}"
-
-    search_iterator = catalog.search(
-        data_collection,
-        bbox=bbox,
-        time=time_interval,
-        filter=cloud_cover_query,
-        fields={
-            "include": [
-                "properties.datetime",
-            ],
-            "exclude": [],
-        },
-    )
-
-    return filter_times(search_iterator.get_timestamps(), time_difference)
+MODEL_BANDS = [S2_BANDS[band_idx] for band_idx in MODEL_BAND_IDS]
 
 
 # pylint: disable-msg=too-many-locals
@@ -122,10 +29,21 @@ def download_bands_and_valid_data_mask(
 
     client = SentinelHubDownloadClient(config=config)
 
+    bands_output = "bands"
+    data_mask_output = "dataMask"
+    bands = S2_BANDS if all_bands else MODEL_BANDS
+    evalscript = generate_evalscript(
+        data_collection=data_collection,
+        bands=bands,
+        meta_bands=[data_mask_output],
+        merged_bands_output=bands_output,
+        prioritize_dn=True,
+    )
+
     api_requests = []
     for timestamp in timestamps:
         request = SentinelHubRequest(
-            evalscript=get_s2_evalscript(all_bands=all_bands, reflectance=False),
+            evalscript=evalscript,
             input_data=[
                 SentinelHubRequest.input_data(
                     data_collection=data_collection,
@@ -133,7 +51,8 @@ def download_bands_and_valid_data_mask(
                 )
             ],
             responses=[
-                SentinelHubRequest.output_response("bands", MimeType.TIFF),
+                SentinelHubRequest.output_response(bands_output, MimeType.TIFF),
+                SentinelHubRequest.output_response(data_mask_output, MimeType.TIFF),
                 SentinelHubRequest.output_response("userdata", MimeType.JSON),
             ],
             bbox=bbox,
@@ -148,9 +67,8 @@ def download_bands_and_valid_data_mask(
 
     bands, mask = [], []
     for response in responses:
-        data = response["bands.tif"]
-        bands.append(data[..., :-1] * response["userdata.json"]["norm_factor"])
-        mask.append(data[..., -1])
+        bands.append(response[f"{bands_output}.tif"] * response["userdata.json"]["norm_factor"])
+        mask.append(response[f"{data_mask_output}.tif"])
 
     return np.array(bands, dtype=np.float32), np.array(mask, dtype=bool)
 
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,60 +1,15 @@
 import datetime as dt
-from typing import List, Tuple
+from typing import Tuple
 
 import numpy as np
 import pytest
 
 from sentinelhub import CRS, BBox
 
-from s2cloudless.utils import download_bands_and_valid_data_mask, get_s2_evalscript, get_timestamps
+from s2cloudless.utils import download_bands_and_valid_data_mask
 
 BBOX1 = BBox((-90.9216499, 14.4190528, -90.8186531, 14.5520163), crs=CRS.WGS84)
 BBOX2 = BBox(((620000, 8210000), (660000, 8270000)), crs=CRS(32738))
-
-
-@pytest.mark.parametrize("all_bands", [True, False])
-@pytest.mark.parametrize("reflectance", [True, False])
-def test_get_s2_evalscript(all_bands: bool, reflectance: bool) -> None:
-    evalscript = get_s2_evalscript(all_bands=all_bands, reflectance=reflectance)
-
-    assert isinstance(evalscript, str)
-
-    bands_num_str = "bands: 14" if all_bands else "bands: 11"
-    assert bands_num_str in evalscript
-
-    input_units_str = 'units: "reflectance"' if reflectance else 'units: "DN"'
-    assert input_units_str in evalscript
-    output_sample_type_str = 'sampleType: "FLOAT32"' if reflectance else 'sampleType: "UINT16"'
-    assert output_sample_type_str in evalscript
-
-
-@pytest.mark.parametrize(
-    "test_input, expected",
-    [
-        ({"bbox": BBOX1, "time_interval": (dt.datetime(2021, 1, 1), dt.datetime(2021, 1, 1))}, []),
-        (
-            {"bbox": BBOX1, "time_interval": (dt.datetime(2021, 1, 1), dt.datetime(2021, 1, 10))},
-            [
-                dt.datetime(2021, 1, 4, 16, 39, 29),
-                dt.datetime(2021, 1, 4, 16, 39, 44),
-                dt.datetime(2021, 1, 9, 16, 39, 29),
-                dt.datetime(2021, 1, 9, 16, 39, 43),
-            ],
-        ),
-        (
-            {
-                "bbox": BBOX1,
-                "time_interval": (dt.datetime(2021, 1, 1), dt.datetime(2021, 1, 10)),
-                "time_difference": dt.timedelta(seconds=600),
-            },
-            [dt.datetime(2021, 1, 4, 16, 39, 29), dt.datetime(2021, 1, 9, 16, 39, 29)],
-        ),
-    ],
-)
-@pytest.mark.sh_integration
-def test_get_timestamps(test_input: dict, expected: List[dt.datetime]) -> None:
-    timestamps = get_timestamps(**test_input)
-    assert [timestamp.replace(tzinfo=None) for timestamp in timestamps] == expected
 
 
 @pytest.mark.parametrize(
